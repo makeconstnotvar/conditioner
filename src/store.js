aa57cd1ff398cb7fbbb74763b9e593e0df55c2c1
Alexander <makeconstnotvar@gmail.com>
2025-09-30 22:42:44 +0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -4,9 +4,9 @@ export class ConditionStore {
   @observable accessor  isSwitched = false;
   @observable accessor isWindow = false;
   @observable accessor isElectric = false;
-  @observable accessor isCentral = false;
+  @observable accessor isHeating = false;
 
   @computed get isEnabled() {
-    return this.isSwitched && this.isWindow && this.isElectric && this.isCentral;
+    return this.isSwitched && !this.isWindow && this.isElectric && !this.isHeating;
   }
 }
