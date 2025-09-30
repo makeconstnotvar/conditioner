07111e12d38faff0aa10cdfaf7eff8970e425f41
Alexander <makeconstnotvar@gmail.com>
2025-09-30 22:32:31 +0300

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,11 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'mobx-react';
 import { ConditionStore } from './store';
 import { App } from './App';
+import { configure } from 'mobx';
+
+configure({
+  enforceActions:'never'
+})
 
 const stores = {
   $condition: new ConditionStore()
